517063aa73bfda0160f33fb201c84e3329761c3e
Jeremy Krumsick <jmkrumsick@gmail.com>
2018-02-19 16:27:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay(musicians, instruments){
+  var = total = [];
+  for (i = 0; i < musicians.lengt; i++){
+    var musician = musicians[i]
+    var instruments = instruments[i]
+    var combo = musician + " plays " instruments;
+    total.push(combo);
+  }
+  return total;
+}
