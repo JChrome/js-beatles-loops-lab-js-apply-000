e256a9e078228973cd2d28cf9b87aa5e8eb72e53
Jeremy Krumsick <jmkrumsick@gmail.com>
2018-02-19 16:34:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var total = [];
+  var i = 0;
   for (i = 0; i < musicians.length; i++){
     var musician = musicians[i]
     var instrument = instruments[i]
