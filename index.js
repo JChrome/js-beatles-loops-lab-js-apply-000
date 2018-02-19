6af4629f87a30f85139cd80446dc4f623986fb7d
Jeremy Krumsick <jmkrumsick@gmail.com>
2018-02-19 16:29:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var total = [];
-  for (i = 0; i < musicians.lengt; i++){
+  for (i = 0; i < musicians.length; i++){
     var musician = musicians[i]
     var instruments = instruments[i]
     var combo = `${musician} plays ${instruments}`
